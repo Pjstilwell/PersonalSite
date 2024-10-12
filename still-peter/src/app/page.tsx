24296dd0c84e7d3740aa7480b82c8e812bdd3184
Peter Stilwell <pjstilwell99@gmail.com>
2024-10-12 18:21:51 +0100

    Created grid, seeing key error

--- still-peter/src/app/page.tsx
+++ still-peter/src/app/page.tsx
@@ -1,101 +1,68 @@
+'use client';
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  let numRows: number = 5;
+  let numCols: number = 5;
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  let iteration = createIterationArray(numRows, numCols);
+
+  const squareClickedParent = (squareIndexRow: number, squareIndexCol: number) => {
+    console.log(squareIndexRow);
+    console.log(squareIndexCol);
+  };
+
+  let rows = [];
+  for (let i = 0; i < numRows; i++) {
+    const row = [];
+    for (let j = 0; j < numCols; j++) {
+      let props = {
+        squareKey: i + '-' + j,
+        stateInput: iteration[i][j],
+        squareClickedParent: squareClickedParent,
+        squareIndexRow: i,
+        squareIndexCol: j,
+      };
+      const key = i + '-' + j;
+      row.push(<Square {...props}/>);
+    }
+    rows.push(<div className="grid-wrapper" key={i}>{row}</div>);
+  }
+
+  return <div className="page-wrapper">{rows}</div>;
+}
+
+function Square(props: any) {
+    const [state, setState] = useState(props.stateInput);
+
+    function squareClicked() {
+      setState(!state);
+    }
+
+    return (
+      <div className="square-wrapper" key={props.squareKey}>
+        <button
+          className="square-button"
+          style={{ backgroundColor: state ? "black" : "white" }}
+          onClick={() => {props.squareClickedParent(props.squareIndexRow, props.squareIndexCol); squareClicked()}
+          }
+        ></button>
+      </div>
+    );
+  }
+
+function createIterationArray(numRows: number, numCols: number): boolean[][] {
+  let iteration: boolean[][] = [];
+  let iterationRow: boolean[] = [];
+
+  for (let i = 0; i < numCols; i++) {
+    iterationRow.push(false);
+  }
+
+  for (let i = 0; i < numRows; i++) {
+    iteration.push(iterationRow);
+  }
+
+  return iteration;
 }
