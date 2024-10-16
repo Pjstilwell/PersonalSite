9ddc3876bab162d53de65681767912e36db1156f
Peter Stilwell <pjstilwell99@gmail.com>
2024-10-16 23:00:39 +0100

    Added settings sidenav and title

--- still-peter/src/app/page.tsx
+++ still-peter/src/app/page.tsx
@@ -123,23 +123,27 @@ function Grid(props: any) {
 function Controls(props: any) {
   return (
     <div className="control-wrapper">
-      <button onClick={() => props.goClicked()}>go</button>
-      numRows: 
+      <h1 className="title">Game of Life</h1>
+      <div className="settings-wrapper"><button onClick={() => props.goClicked()}>go</button>
+      <div className="flexSetting"><p>numRows:</p>
       <input
         type="number"
         min={0}
         max={100}
         value={props.numRows}
         onChange={(e) => props.numRowsChanged(e.target.value)}
-      ></input>
-      numCols: 
+      ></input></div>
+      <div className="flexSetting"><p>numRows:</p>
       <input
         type="number"
         min={0}
         max={100}
         value={props.numCols}
         onChange={(e) => props.numColsChanged(e.target.value)}
-      ></input>
+      ></input></div>
+      </div>
+      
+      
 
 <button onClick={() => props.randomise()}>randomise</button>
     </div>
