a06cc7544a139d2f853d8d2e8ca7a9128cdbd405
Peter Stilwell <pjstilwell99@gmail.com>
2024-10-19 22:42:20 +0100

    Added a bit of colour, sass, and UI

--- still-peter/src/app/components/controls.tsx
+++ still-peter/src/app/components/controls.tsx
@@ -1,32 +1,49 @@
 export default function Controls(props: any) {
-    return (
-      <div className="control-wrapper">
-        <h1 className="title">Game of Life</h1>
-        <div className="settings-wrapper">
-          <button onClick={() => props.goClicked()}>go</button>
-          <div className="flexSetting">
-            <p>numRows:</p>
-            <input
-              type="number"
-              min={0}
-              max={100}
-              value={props.numRows}
-              onChange={(e) => props.numRowsChanged(e.target.value)}
-            ></input>
-          </div>
-          <div className="flexSetting">
-            <p>numRows:</p>
-            <input
-              type="number"
-              min={0}
-              max={100}
-              value={props.numCols}
-              onChange={(e) => props.numColsChanged(e.target.value)}
-            ></input>
-          </div>
+  return (
+    <div className="control-wrapper">
+      <h1 className="title">Game of Life</h1>
+      <div className="settings-wrapper">
+        <div className="scrub-wrapper">
+          <button>
+            <span className="material-symbols-outlined">first_page</span>
+          </button>
+          <button>
+            <span className="material-symbols-outlined">play_pause</span>
+          </button>
+          <button onClick={() => props.goClicked()}>
+            <span className="material-symbols-outlined">last_page</span>
+          </button>
         </div>
-  
-        <button onClick={() => props.randomise()}>randomise</button>
+        <div className="randomise-wrapper">
+          <button onClick={() => props.randomise()}>
+            <span className="material-symbols-outlined casino-logo-spacing">
+              casino
+            </span>
+            Randomise
+          </button>
+        </div>
+
+        <div className="row-col-controls-wrapper">
+          <p id="row-para">Rows:</p>
+          <input
+            id="row-input"
+            type="number"
+            min={0}
+            max={100}
+            value={props.numRows}
+            onChange={(e) => props.numRowsChanged(e.target.value)}
+          ></input>
+          <p id="col-para">Columns:</p>
+          <input
+            id="col-input"
+            type="number"
+            min={0}
+            max={100}
+            value={props.numCols}
+            onChange={(e) => props.numColsChanged(e.target.value)}
+          ></input>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
--- still-peter/src/app/components/square.tsx
+++ still-peter/src/app/components/square.tsx
@@ -1,29 +1,34 @@
 import { useEffect, useState } from "react";
 
 export default function Square(props: any) {
-    const [squareState, setSquareState] = useState<boolean | null>(null);
-  
-    useEffect(() => {
-      setSquareState(props.stateInput);
-    }, [props]);
-  
-    function squareClicked() {
-      let newVal = !squareState;
-      setSquareState(newVal);
-  
-      //tell Home
-      props.squareClicked(props.squareIndexRow, props.squareIndexCol, newVal);
-    }
-  
-    return (
-      <div className="square-wrapper" key={props.squareKey} style={{ width: `calc(100% / ${props.numCols})`}}>
-        <button
-          className="square-button"
-          style={{ backgroundColor: squareState ? "black" : "white" }}
-          onClick={() => {
-            squareClicked();
-          }}
-        ></button>
-      </div>
-    );
+  const [squareState, setSquareState] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    setSquareState(props.stateInput);
+  }, [props]);
+
+  function squareClicked() {
+    let newVal = !squareState;
+    setSquareState(newVal);
+
+    //tell Home
+    props.squareClicked(props.squareIndexRow, props.squareIndexCol, newVal);
   }
+
+  const stateClass = squareState ? "square-on" : "square-off";
+
+  return (
+    <div
+      className="square-wrapper"
+      key={props.squareKey}
+      style={{ width: `calc(100% / ${props.numCols})` }}
+    >
+      <button
+        className={"square-button " + stateClass}
+        onClick={() => {
+          squareClicked();
+        }}
+      ></button>
+    </div>
+  );
+}
