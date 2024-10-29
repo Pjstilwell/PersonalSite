5f1934bcb64f4e403b97057cfa3ff2b36cf3f53d
Peter Stilwell <pjstilwell99@gmail.com>
2024-10-29 22:07:35 +0000

    Added more patterns and overflow styling. Bit buggy and need to validate adding patterns to the grid

--- still-peter/src/app/components/controls.tsx
+++ still-peter/src/app/components/controls.tsx
@@ -83,80 +83,83 @@ export default function Controls(props: ControlProps) {
   return (
     <div className="control-wrapper">
       {overlay}
-      <div className="title-wrapper">
-        <h1 className="title">GAME OF LIFE</h1>
+      <div className="settings-big-wrapper">
+        <div className="title-wrapper">
+          <h1 className="title">GAME OF LIFE</h1>
+        </div>
+
+        <div className="settings-wrapper">
+          <div className="scrub-wrapper">
+            <button
+              disabled={props.iterationsLength < 2}
+              onClick={() => props.backClicked()}
+            >
+              <span className="material-symbols-outlined">first_page</span>
+            </button>
+            <button
+              disabled={!props.activeCells || props.seqTerminated}
+              className={playClickedClass}
+              onClick={() => props.togglePlaying()}
+            >
+              <span className="material-symbols-outlined">play_pause</span>
+            </button>
+            <button
+              disabled={!props.activeCells || props.seqTerminated}
+              onClick={() => props.goClicked()}
+            >
+              <span className="material-symbols-outlined">last_page</span>
+            </button>
+          </div>
+          <div className="button-wrapper">
+            <button disabled={props.playing} onClick={() => props.randomise()}>
+              <span className="material-symbols-outlined casino-logo-spacing">
+                casino
+              </span>
+              <p>Randomise</p>
+            </button>
+            <button
+              disabled={!props.activeCells || props.playing}
+              onClick={() => props.clear()}
+            >
+              <span className="material-symbols-outlined casino-logo-spacing">
+                check_box_outline_blank
+              </span>
+              <p>Clear</p>
+            </button>
+          </div>
+
+          <div className="row-col-controls-wrapper">
+            <div className="flexy">
+              <p id="row-para">Rows (1-100):</p>
+            </div>
+            <input
+              id="row-input"
+              type="number"
+              min={0}
+              max={100}
+              value={props.numRows}
+              onChange={(e) => props.numRowsChanged(e.target.value)}
+            ></input>
+            <div className="flexy">
+              <p id="col-para">Columns (1-100):</p>
+            </div>
+            <input
+              id="col-input"
+              type="number"
+              min={0}
+              max={100}
+              value={props.numCols}
+              onChange={(e) => props.numColsChanged(e.target.value)}
+            ></input>
+          </div>
+        </div>
       </div>
-
-      <div className="settings-wrapper">
-        <div className="scrub-wrapper">
-          <button
-            disabled={props.iterationsLength < 2}
-            onClick={() => props.backClicked()}
-          >
-            <span className="material-symbols-outlined">first_page</span>
-          </button>
-          <button
-            disabled={!props.activeCells || props.seqTerminated}
-            className={playClickedClass}
-            onClick={() => props.togglePlaying()}
-          >
-            <span className="material-symbols-outlined">play_pause</span>
-          </button>
-          <button
-            disabled={!props.activeCells || props.seqTerminated}
-            onClick={() => props.goClicked()}
-          >
-            <span className="material-symbols-outlined">last_page</span>
-          </button>
-        </div>
-        <div className="button-wrapper">
-          <button disabled={props.playing} onClick={() => props.randomise()}>
-            <span className="material-symbols-outlined casino-logo-spacing">
-              casino
-            </span>
-            <p>Randomise</p>
-          </button>
-          <button
-            disabled={!props.activeCells || props.playing}
-            onClick={() => props.clear()}
-          >
-            <span className="material-symbols-outlined casino-logo-spacing">
-              check_box_outline_blank
-            </span>
-            <p>Clear</p>
-          </button>
-        </div>
-
-        <div className="row-col-controls-wrapper">
-          <div className="flexy">
-            <p id="row-para">Rows (1-100):</p>
-          </div>
-          <input
-            id="row-input"
-            type="number"
-            min={0}
-            max={100}
-            value={props.numRows}
-            onChange={(e) => props.numRowsChanged(e.target.value)}
-          ></input>
-          <div className="flexy">
-            <p id="col-para">Columns (1-100):</p>
-          </div>
-          <input
-            id="col-input"
-            type="number"
-            min={0}
-            max={100}
-            value={props.numCols}
-            onChange={(e) => props.numColsChanged(e.target.value)}
-          ></input>
-        </div>
+      <div className="patterns-settings-wrapper">
         <div className="add-patterns-text-wrapper">
           <p>Add Patterns</p>
         </div>
         <div className="patterns-wrapper">{patternList}</div>
       </div>
-      {/* <div className="pattern-display-wrapper">{displayedPattern}</div> */}
     </div>
   );
 }
--- still-peter/src/app/resources/Patterns.ts
+++ still-peter/src/app/resources/Patterns.ts
@@ -1,6 +1,13 @@
 //assuming starting block (top left) is 0-0
 //handler of these patterns should add the coordinates to the actual starting index
 export enum Patterns {
-  block = "block",
-  beehive = "beehive",
+  Block = "Block",
+  Beehive = "Beehive",
+  Loaf = "Loaf",
+  Boat = "Boat",
+  Tub = "Tub",
+  Blinker = "Blinker",
+  Toad = "Toad",
+  Beacon = "Beacon",
+  Pulsar = "Pulsar",
 }
