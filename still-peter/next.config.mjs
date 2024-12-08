efd318ac122eb54b213479e89459a31fdba4bf60
Peter Stilwell <pjstilwell99@gmail.com>
2024-12-08 11:52:10 +0000

    Deployment Configuration

--- still-peter/next.config.mjs
+++ still-peter/next.config.mjs
@@ -1,5 +1,8 @@
-export default {
-  output: "export", // Enables static export
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
   basePath: "/PersonalSite", // Set base path for GitHub Pages
   assetPrefix: "/PersonalSite/", // Ensure assets are loaded from the right URL
 };
+/* module.exports = nextConfig*/
+export default nextConfig;
